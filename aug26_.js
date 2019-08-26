2cbd479d525c95450b96bac64e5aa3d97bfb103b
cudjoeab <cudjoeab@gmail.com>
2019-08-26 14:43:33 -0400

    pseudo code solution;

---
+++ aug26_.js
@@ -0,0 +1,20 @@
+// 003111 # 3 = 1 + 1 + 1
+// 813372 # 8 + 1 + 3 = 3 + 7 + 2
+// 17935 # 1 + 7 = 3 + 5
+// 56328116
+
+/* write a method luckCheck(str), 
+which returns true if argument is string decimal representation of a lucky ticket number,
+or false for all other numbers. 
+It should handle errors for empty strings or strings which don't represent a decimal number*/
+
+function luckCheck(str) {
+// split the string into to equal halves 
+// take the sum of the first half 
+// take the sum of the second half 
+// compare the sum of the first half to the sum of the second half 
+// if true, return "luck number!"
+// if false, return "regular number!" 
+}; 
+
+luckCheck(56328116); 
